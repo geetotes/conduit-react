f786d16b120ee7a0f38c3fc413092e105e33608d
Lee Gillentine <lee@fishburd.com>
2017-05-20 13:59:53 -0700

    Mocking in components

---
+++ src/Books.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Books extends Component {
+  render() {
+    return(
+      <div>
+        <span>General Petraus Book</span>
+        <span>General Smith Book</span>
+      </div>
+    );
+  }
+}
+
+export default Books;
