336643b9545d29f83ff57318ede0d2e827848d5d
yichiang <yi_chiang@Outlook.com>
2017-05-20 21:08:16 -0700

    update detail

---
+++ src/IconBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './MainPage.css';
+import 'semantic-ui-css/semantic.min.css';
+import * as moment from 'moment';
+// import './../public/css/font-awesome.min.css';
+
+class IconBar extends Component {
+
+
+  render() {
+  
+    return (
+
+      <div>
+        <div className="ui labeled button" tabindex="0">
+        <div className="ui red button">
+          <i className="heart icon"></i> Like
+        </div>
+        <a className="ui basic red left pointing label">
+          1,048
+        </a>
+      </div>
+      <div className="ui labeled button" tabindex="0">
+        <div className="ui basic blue button">
+          <span className="fa fa-eye" aria-hidden="true"></span> View
+        </div>
+        <a className="ui basic left pointing blue label">
+          1,048
+        </a>
+      </div>
+    </div>
+
+    );
+  }
+}
+
+export default IconBar;
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import './MainPage.css';
 import 'semantic-ui-css/semantic.min.css';
-
-
+import * as moment from 'moment';
+// import './../public/css/font-awesome.min.css';
+import IconBar from './IconBar'
 class MainPage extends Component {
   constructor(props) {
   super(props);
   this.state = { newsData: null};
   }
   componentWillMount(){
-    var self =this;
+    var self = this;
     const id = this.props.match.params.newsId;
     const newsMain = this.props.newsActionsTemp.getNewsbyId(id);
     newsMain.once('value').then(function(snapshot) {
@@ -22,12 +23,23 @@ class MainPage extends Component {
 
   render() {
     console.log("props", this.props);
-
+    const news = this.state.newsData;
     return (
 
       <div>
         <h1>MainPage</h1>
-        <h2>{this.state.newsData? this.state.newsData.title : "No new was found"}</h2>
+        <div className="mainPanel">
+        {news&& <img className="mainImg" src={news.urlToImage} />  }
+        {/* <img src={this.news.urlToImage} /> */}
+        <h2>{news? news.title : "No new was found"}</h2>
+        <h5>{news? news.description : "No new was found"}</h5>
+        <h5>{news? news.source : "No new was found"}</h5>
+        <div className="row footerBar">
+          <IconBar/>
+        <h5 className="timeTag">{news? moment(news.publishedAt).fromNow() : "No new was found"}</h5>
+        {/* <span className="fa fa-eye" aria-hidden="true"></span> */}
+        </div>
+      </div>
 
       </div>
     );
