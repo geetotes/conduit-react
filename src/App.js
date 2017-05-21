e98ec07041f88011197ebe435ca5df68907235aa
Lee Gillentine <lee@fishburd.com>
2017-05-20 18:00:03 -0700

    Adding feedback widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 import logo from './logo.svg';
 import List from './List.js';
 import './App.css';
@@ -15,9 +15,23 @@ class App extends Component {
     const { activeItem } = this.state;
     let news = this.props.news;
     if (news === undefined) {
-      alert('Error with firebase');
+      alert('Error with firebase -- news is undefined');
       return;
     }
+
+    if (this.props.users === undefined) {
+      alert('Error with firebase -- users are undefined');
+      return;
+    }
+
+    let books = this.props.users.map((u) => {
+      return {
+        key: `book-list-${u._id}`,
+        value: u._id,
+        text: u.name,
+      };
+    });
+
 
     console.log("props", this.props);
     return (
@@ -26,8 +40,19 @@ class App extends Component {
           <Menu.Item header>Today's Shit</Menu.Item>
           <Menu.Item name='newest' active={activeItem === 'newest'} onClick={this.handleItemClick} />
           <Menu.Item name='hottest' active={activeItem === 'hottest'} onClick={this.handleItemClick} />
+          <Menu.Item name='yesterday' active={activeItem === 'yesterday'} onClick={this.handleItemClick} />
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Dropdown 
+                placeholder='Book'
+                multiple
+                search
+                selection
+                options={books} />
+            </Menu.Item>
+          </Menu.Menu>
         </Menu>
-        <List items={news}/>
+        <List items={news} activeItem={activeItem}/>
         {/* <ExampleComponent /> */}
       </div>
     );
--- src/List.js
+++ src/List.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import Tags from './Tags.js';
 import Books from './Books.js';
-import { Grid, Image, Label } from 'semantic-ui-react'
+import { Grid, Image, Label, Dropdown, Icon } from 'semantic-ui-react'
 
 class List extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     let items = this.props.items;
+    let feedback = null;
+    if (this.props.activeItem === 'yesterday') {
+      let options = [
+        {
+          key: 1,
+          value: 'good',
+          text: 'Useful',
+          content: <Icon name='smile' color='green'/>
+        },
+        {
+          key: 2,
+          value: 'meh',
+          text: 'Meh',
+          content: <Icon name='meh' color='grey'/>
+        },
+        {
+          key: 3,
+          value: 'bad',
+          text: 'Useless',
+          content: <Icon name='frown' color='red'/>
+        }
+      ];
+      feedback = (
+        <Dropdown style={{float: 'right'}} selection options={options} placeholder='Feedback' />
+      );
+    }
 
     return (
       <Grid celled>
         {items.map((d,i) => {
           return (
-            <Grid.Row>
+            <Grid.Row key={`grid-item-${i}`}>
               <Grid.Column width={3}>
                 <Image src={d.urlToImage}/>
               </Grid.Column>
               <Grid.Column width={13}>
                 <Label as='a' color='red' ribbon>{d.title}</Label>
+                {feedback}
                 <p>{d.publishedAt}</p>
                 <p>{d.description}</p>
                 <Tags key={`tags-$i`} icon={'tags'} tags={d.tags}/>
                 <Tags key={`users-$i`} icon={'user circle outline'} tags={d.tags}/>
-                <Books/>
               </Grid.Column>
             </Grid.Row>
           );
