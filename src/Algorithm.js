ed06a420a6a3938d1dbefa4f67f427a8dae06389
ckubec <ckubec@gmail.com>
2017-05-21 01:45:37 -0700

    Basic framework for algorithm class. Was not able to test due to errors with React and base JavaScript not being friendly with each other.

---
+++ src/Algorithm.js
@@ -0,0 +1,43 @@
+/**
+ * Simple sorting priority Algorithm.
+ *
+ * @type {{}}
+ */
+var hash = {};
+var score = 0;
+var scoreCards = [];
+
+function algFunction(store) {
+    getKeys(store.userBooks);
+    setOrder(store.news);
+
+    return store;
+}
+
+/*
+Assigns tags to a key pair
+ */
+function getKeys(thekeys){
+    for (var i=0; i<thekeys.size; i++){
+        hash[thekeys[i]] = thekeys;
+    }
+}
+
+function executeOrdering(user, articles) {
+    setOrder(getImportance(user), articles);
+    return
+}
+
+/*
+Finding the order of articles
+ */
+function setOrder(articles) {
+    for(var i=0; i<articles.size; i++){
+        for(var j=0; j<articles.Object.tags.size; j++){
+            if(hash[articles.Object.tags[j]] === true) {
+                score += hash[articles.Object.tags[j]];
+            }
+        }
+    }
+}
+
